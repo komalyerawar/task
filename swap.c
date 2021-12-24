911332cab2ffe2ff04f4af36fe465bba16adf656
Komal Prakash Yerawar <91716387+komalyerawar@users.noreply.github.com>
2021-12-24 18:23:56 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+        int a,b;
+        printf("Enter a number\n");
+        scanf("%d %d",&a,&b);
+        if(a!=b)
+        {
+                a=a^b;
+                b=a^b;
+                a=a^b;
+        }
+        /*      if(a!=b)
+        {
+                a=a*b;
+                b=a/b;
+                a=a/b;
+        }
+                if(a!=b)
+        {
+                a=a+b;
+                b=a-b;
+                a=a-b;
+        }
+*/      printf("%d %d\n",a,b);
+}
